ed08f37f422ee526bf3b8167e8d19fc8fa26940a
RupankarGarai2003 <rupankargarai4@gmail.com>
2026-05-30 23:04:40 +0530

    checking

---
+++ new.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args){
+        int a =10;
+        int b =20;
+        int sum = a+b;
+        System.out.println(sum);
+        if(sum%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+
+        for(int i=1;i<10;i++){
+            System.out.println(i);
+        }
+    }
+}
