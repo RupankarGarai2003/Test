d5fddec9cbd8c441e7e98da0c429c14c0da1f592
Badsha <a@gmail.com>
2026-05-31 19:20:09 +0530

    check

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args){
+        int age = 23;
+        String name = "Badsha";
+        System.out.println("Age: "+ age + "   " + "Name:"+name);
+
+        int n1=10;
+        int n2=20;
+        int sum = n1+n2;
+        System.out.println("Sum: " + sum);
+}
+}
